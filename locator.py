9a9b3581d492700c43b7092663cefcc018076993
Anna Krylova <msshinkei@gmail.com>
2023-04-11 18:38:05 +0300

    removed extra variables and changed locators

--- locator.py
+++ locator.py
@@ -4,11 +4,11 @@ class Locator:
     profile_button = "//p[contains(text(),'Личный Кабинет')]"   #кнопка входа в Личный кабинет
     login_button = ".//button[contains(text(),'Войти в аккаунт')]"   #кнопка входа в аккаунт
     registration_link = ".//a[contains(text(), 'Зарегистрироваться')]"  #кнопка регистрации
-    registration_email = "//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[2]/div[1]/div[1]/input[1]"  #поле ввода почты на форме регистрации
-    registration_password = "//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[3]/div[1]/div[1]/input[1]"  # поле ввода пароля на форме регистрации
+    registration_email = ".//fieldset[2]/div[1]/div[1]/input[@class='text input__textfield text_type_main-default']"  #поле ввода почты на форме регистрации
+    registration_password = ".//input[@name='Пароль']"  # поле ввода пароля на форме регистрации
     registration_button = "//button[contains(text(),'Зарегистрироваться')]"  # кнопка регистрации на форме регистрации
     registration_title = "//h2[contains(text(),'Регистрация')]"  #заголовок формы регистрации
-    name = "//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[1]/div[1]/div[1]/input[1]"  # поле для ввода имени на форме регистрации
+    name = ".//fieldset[1]/div[1]/div[1]/input[@class='text input__textfield text_type_main-default']"  # поле для ввода имени на форме регистрации
     login_title = ".//h2[contains(text(),'Вход')]"  #заголовок формы авторизации
     authorization_email = ".//form/fieldset[1]/div/div[1]/input"  #поле ввода почты на форме авторизации
     authorization_password = ".//form/fieldset[2]/div/div[1]/input" # поле ввода пароля на форме авторизации
@@ -20,8 +20,8 @@ class Locator:
     profile_link = "//a[contains(text(),'Профиль')]"  # кнопка "Профиль" в личном кабинете
     constructor_button = "//p[contains(text(),'Конструктор')]"    # кнопка "Конструктор" на главной странице
     logo = "//header/nav[1]/div[1]/a[1]/*[1]"   # логотип "Stellar Burgers"
-    sauces = "//body/div[@id='root']/div[1]/main[1]/section[1]/div[1]/div[2]"  # кнопка "Соусы"
-    buns = "//body/div[@id='root']/div[1]/main[1]/section[1]/div[1]/div[1]"   # кнопка "Булки"
-    toppings = "//body/div[@id='root']/div[1]/main[1]/section[1]/div[1]/div[3]"    # кнопка "Начинки"
+    sauces = ".//span[contains(text(),'Соусы')]"  # кнопка "Соусы"
+    buns = ".//span[contains(text(),'Булки')]"   # кнопка "Булки"
+    toppings = ".//span[contains(text(),'Начинки')]"   # кнопка "Начинки"
     logout_button = "//button[contains(text(),'Выход')]"    # кнопка деавторизации
     incorrect_pass = "//p[contains(text(),'Некорректный пароль')]"    # сообщение о некорректном пароле
--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -9,53 +9,44 @@ unclicked_toppings = 'tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect'
 
 class TestProfileOpen:
     def test_profile_page_open(self, login):
-        driver = login
-        driver.find_element(By.XPATH, Locator.profile_button).click()
-        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.profile_link)))
-        title = driver.find_element(By.XPATH, Locator.profile_link).text
+        login.find_element(By.XPATH, Locator.profile_button).click()
+        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.profile_link)))
+        title = login.find_element(By.XPATH, Locator.profile_link).text
         assert title == "Профиль"
 
 
     def test_from_profile_to_constructor_by_button(self, login):
-        driver = login
-        driver.find_element(By.XPATH, Locator.profile_button).click()
-        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.profile_link)))
+        login.find_element(By.XPATH, Locator.profile_button).click()
+        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.profile_link)))
 
-        driver.find_element(By.XPATH, Locator.constructor_button).click()
-        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.constructor_title)))
-        title = driver.find_element(By.XPATH, Locator.constructor_title).text
+        login.find_element(By.XPATH, Locator.constructor_button).click()
+        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.constructor_title)))
+        title = login.find_element(By.XPATH, Locator.constructor_title).text
         assert title == "Соберите бургер"
 
     def test_from_profile_to_constructor_by_logo(self, login):
-        driver = login
-        driver.find_element(By.XPATH, Locator.profile_button).click()
-        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.profile_link)))
+        login.find_element(By.XPATH, Locator.profile_button).click()
+        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.profile_link)))
 
-        driver.find_element(By.XPATH, Locator.logo).click()
-        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.constructor_title)))
-        title = driver.find_element(By.XPATH, Locator.constructor_title).text
+        login.find_element(By.XPATH, Locator.logo).click()
+        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.constructor_title)))
+        title = login.find_element(By.XPATH, Locator.constructor_title).text
         assert title == "Соберите бургер"
 
     def test_constructor_to_sauces_navigation(self, login):
-        driver = login
-
-        driver.find_element(By.XPATH, Locator.sauces).click()
-        clicked_sauces = driver.find_element(By.XPATH, Locator.sauces).get_attribute('class')
+        login.find_element(By.XPATH, Locator.sauces).click()
+        clicked_sauces = login.find_element(By.XPATH, Locator.sauces).get_attribute('class')
         assert clicked_sauces != unclicked_sauces
 
     def test_constructor_to_buns_navigation(self, login):
-        driver = login
-
-        driver.find_element(By.XPATH, Locator.sauces).click()
-        driver.find_element(By.XPATH, Locator.buns).click()
-        clicked_buns = driver.find_element(By.XPATH, Locator.buns).get_attribute('class')
+        login.find_element(By.XPATH, Locator.sauces).click()
+        login.find_element(By.XPATH, Locator.buns).click()
+        clicked_buns = login.find_element(By.XPATH, Locator.buns).get_attribute('class')
         assert clicked_buns != unclicked_buns
 
     def test_constructor_to_toppings_navigation(self, login):
-        driver = login
-
-        driver.find_element(By.XPATH, Locator.toppings).click()
-        clicked_toppings = driver.find_element(By.XPATH, Locator.toppings).get_attribute('class')
+        login.find_element(By.XPATH, Locator.toppings).click()
+        clicked_toppings = login.find_element(By.XPATH, Locator.toppings).get_attribute('class')
         assert clicked_toppings != unclicked_toppings
 
 
