9e064e37776d07a1db1553259f312b5601302e98
Anna Krylova <msshinkei@gmail.com>
2023-04-11 16:50:48 +0300

    deleted extra variable

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -5,10 +5,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 class TestProfileLogout:
     def test_profile_logout(self, login):
-        driver = login
-        driver.find_element(By.XPATH, Locator.profile_button).click()
-        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.logout_button)))
-        driver.find_element(By.XPATH, Locator.logout_button).click()
-        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.login_title)))
-        title = driver.find_element(By.XPATH, Locator.login_title).text
+        login.find_element(By.XPATH, Locator.profile_button).click()
+        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.logout_button)))
+        login.find_element(By.XPATH, Locator.logout_button).click()
+        WebDriverWait(login, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, Locator.login_title)))
+        title = login.find_element(By.XPATH, Locator.login_title).text
         assert title == "Вход"
